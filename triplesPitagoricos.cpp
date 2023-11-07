e235c5de09a9ca81909c0051bb51bd9d7b79004a
mllangon <mllangon@myuax.com>
2023-11-07 11:14:53 +0100

    Buscar cuadrados

--- triplesPitagoricos.cpp
+++ triplesPitagoricos.cpp
@@ -6,4 +6,9 @@ int main(){
 
     printf("Ingrese el valor de a: ");
     scanf("%d", &a);
+
+    printf("Ingrese el valor de b: ");
+    scanf("%d", &b);
+
+    double c = sqrt (pow a , 2) + pow(b , 2);
 }

8c1cbcc880fa743f2976ad195fc8bb2379c01638
mllangon <mllangon@myuax.com>
2023-11-07 11:12:44 +0100

    Prueba de ingresar numeros

--- triplesPitagoricos.cpp
+++ triplesPitagoricos.cpp
@@ -2,21 +2,8 @@
 #include<math.h>
 
 int main(){
-    int max_number;
-    printf("Ingrese el numero maximo para buscar triples pitagoricos: ");
-    scanf("%d", &max_number);
+    int a, b;
 
-    printf("Triples Pitagoricos hasta %d: \n",  max_number);
-    int a, b, c;
-
-    for (a = 1; a <= 500; a++){
-        for (b = a; b <=500; b++){
-            c = sqrt (a*a + b*b);
-
-            if (c * c == a*a + b*b & a+b<c){
-                printf("%d,%d,%d\n", a, b, c);
-            }
-        }
-    }
-    return 0;
+    printf("Ingrese el valor de a: ");
+    scanf("%d", &a);
 }
